95d49a6f5a3c9ca57cb78e0e9303827c21e5f077
autumnfish <517729329@qq.com>
2019-06-29 09:00:41 +0800

    login finis

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,9 +1,17 @@
 module.exports = {
-  login(req,res){
+  login(req, res) {
     // 接收数据
-    const {username,password} = req.body
-
-    // 返回
-    res.send('/login')
+    const { username, password } = req.body
+    // 正确性判断
+    if (username == 'admin' && password == '123456') {
+      return res.send({
+        msg: '登录成功',
+        code: 200
+      })
+    }
+    return res.send({
+      msg: '用户名或密码错误',
+      code: 400
+    })
   }
 }
